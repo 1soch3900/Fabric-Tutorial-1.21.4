dae600376dd0b0b1c45e29f18f9a4440a9a8c90c
1soch3900 <akurah2525mad@gmail.com>
2025-02-27 17:11:08 +0900

    test

--- src/main/java/net/agaaaa/tutorialmod/TutorialMod.java
+++ src/main/java/net/agaaaa/tutorialmod/TutorialMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//comment to check if I successfully set my git up
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
