2506d4dfc69d0e46bf76b02cf20d969c937750bd
1soch3900 <akurah2525mad@gmail.com>
2025-03-17 17:06:15 +0900

    made trapdoors see-through

--- src/main/java/net/agaaaa/tutorialmod/TutorialModClient.java
+++ src/main/java/net/agaaaa/tutorialmod/TutorialModClient.java
@@ -1,10 +1,13 @@
 package net.agaaaa.tutorialmod;
 
+import net.agaaaa.tutorialmod.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RED_AMOGUS_TRAPDOOR, RenderLayer.getCutout());
     }
 }
